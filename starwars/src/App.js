c3208e909977cfd4c39e56c8189dc7cf3faf7fde
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-07-19 16:32:11 +0100

    WIP: answered self-study questions

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import './App.css';
+import styled from 'styled-components'
+
+const testP = styled.p`
+  color: red;
+`
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -12,6 +17,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <testP>placeholder red</testP>
     </div>
   );
 }
