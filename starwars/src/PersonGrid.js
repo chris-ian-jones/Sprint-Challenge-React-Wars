5de0d30c0a13346153d1beb75f39fa9de940f770
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-07-19 16:56:00 +0100

    WIP: setup axios call

--- starwars/src/PersonGrid.js
+++ starwars/src/PersonGrid.js
@@ -1,9 +1,23 @@
 import React, {useEffect, useState} from 'react';
 import styled from 'styled-components'
+import axios from 'axios'
 
 const PersonGrid = () => {
 
-  
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        axios
+            .get('https://swapi.co/api/people/')
+            .then(response => {
+                // const responseData = response.results
+                // setData(responseData)
+                // console.log('a', responseData)
+                console.log(response)
+            })
+
+    }, [])
+    // console.log('b', responseData)
     return (
       <div>
           <p>placeholder text</p>
